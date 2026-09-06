dc04fc628644fc91e0cfcd76c256f61a577dd0a8
schromya <schromya@gmail.com>
2026-09-06 17:36:19 -0500

    Added stylized pixel card

--- pages/home.js
+++ pages/home.js
@@ -32,13 +32,13 @@ function buildBody(bodyId) {
     body.innerHTML = ""; // Clear existing data
     
     body.innerHTML = /*html*/ `
-    
+
         <div class='text-white content-scroll static-height'>
             <!------------------------------------- ABOUT ME -------------------------------------->
             <div class="  m-2 min-static-height">
 
-                <div class="speech-bubble-box speech-bubble-triangle">
-
+            <div class="speech-bubble speech-bubble-triangle">
+                <div class="pixel-card">
                     <div class="text-2xl text-bold font-pixel"> Hey there!</div>
 
                     <div class="text-lg ">
@@ -50,18 +50,23 @@ function buildBody(bodyId) {
                         Before you go, be sure to play a game with me at the bottom of the page.
                     </div>
                 </div>
+            </div>
+
 
                 <div class=" content-right">
                     <img src="assets/FrogTalk.png" class="pb-2">
                 </div>
             </div>
 
-            <!-------------------------------------- PORTFOLIO ------------------------------------>
-            <div class="bg-gray ml-20-md mr-20-md text-md border-rounded-sm-md">
-                <!-------------------------------------- NAV -------------------------------------->
-                <div class=" content-center content-stick-top pt-2 pb-1 border-rounded-sm-md">
 
-                    <!-- <a href="#overviewSection" class="text-white">Overview</a> • -->
+  
+
+
+            <!-------------------------------------- PORTFOLIO ------------------------------------>
+            <div class="pixel-card text-md ">
+                <!-------------------------------------- NAV -------------------------------------->
+                <div class=" content-center content-stick-top pt-2 pb-1">
+
                     <a href="#experienceSection" class="text-white font-pixel">Experience</a> •
                     <a href="#projectsSection" class="text-white font-pixel">Projects</a> •
                     <a href="#educationSection" class="text-white font-pixel">Education</a> •
@@ -69,6 +74,7 @@ function buildBody(bodyId) {
                 </div>
                 <br><br>
 
+                
                 <!------------------------------------ CONTENT ------------------------------------>
 
                 <!------------------------------------- GRAPHS ------------------------------------>
@@ -110,6 +116,9 @@ function buildBody(bodyId) {
                     <ul id="awards" class="text-sm p-0"></ul>
 
                 </div>
+            </div>
+
+
         </div>
     `
 }
